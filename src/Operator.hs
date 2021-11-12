538145e133854474aea7bbb4061e255e79584739
Joshua Hockley <joshua.hockley.dev@protonmail.com>
2021-11-12 16:20:20 +0000

    refactor operator builders for more flexibility in return types

--- src/Operator.hs
+++ src/Operator.hs
@@ -65,103 +65,107 @@ type Operator2 = Value -> Value -> Maybe Value
 --   will coerce values 'down' towards floats if needed
 --   if no handler can be applied to the value(s), the opperator fails
 
-type IntegerOp1  = Integer  -> Integer
-type RationalOp1 = Rational -> Rational
-type FloatingOp1 = Double   -> Double
+type IntegerOp1  = Integer  -> Value
+type RationalOp1 = Rational -> Value
+type FloatingOp1 = Double   -> Value
 
 buildOp1 :: Maybe IntegerOp1  -> Maybe RationalOp1 -> Maybe FloatingOp1 -> Operator1
-buildOp1 (Just iop) _ _  (I i)  = Just . I       . iop                $ i
-buildOp1 _ (Just rop) _  (I i)  = Just . reduceR . rop . fromIntegral $ i
-buildOp1 _ (Just rop) _  (R r)  = Just . reduceR . rop                $ r
-buildOp1 _ _ (Just fop)  v      = Just . F       . fop . asF          $ v
+buildOp1 (Just iop) _ _  (I i)  = Just . iop                $ i
+buildOp1 _ (Just rop) _  (I i)  = Just . rop . fromIntegral $ i
+buildOp1 _ (Just rop) _  (R r)  = Just . rop                $ r
+buildOp1 _ _ (Just fop)  v      = Just . fop . asF          $ v
 buildOp1 _ _ _           _      = Nothing  -- invalid type - fail
 
-type IntegerOp2  = Integer  -> Integer  -> Integer
-type RationalOp2 = Rational -> Rational -> Rational
-type FloatingOp2 = Double   -> Double   -> Double
+type IntegerOp2  = Integer  -> Integer  -> Value
+type RationalOp2 = Rational -> Rational -> Value
+type FloatingOp2 = Double   -> Double   -> Value
 
 buildOp2 :: Maybe IntegerOp2  -> Maybe RationalOp2 -> Maybe FloatingOp2 -> Operator2
 -- integer
-buildOp2 (Just iop) _ _  (I i) (I i') = Just . I       $ iop i i'
+buildOp2 (Just iop) _ _  (I i) (I i') = Just $ iop i i'
 -- rational
-buildOp2 _ (Just rop) _  (I i) (I i') = Just . reduceR $ (rop `on` fromIntegral) i i'
-buildOp2 _ (Just rop) _  (I i) (R r') = Just . reduceR $ rop (fromIntegral i) r'
-buildOp2 _ (Just rop) _  (R r) (I i') = Just . reduceR $ rop r (fromIntegral i')
-buildOp2 _ (Just rop) _  (R r) (R r') = Just . reduceR $ rop r r'
+buildOp2 _ (Just rop) _  (I i) (I i') = Just $ (rop `on` fromIntegral) i i'
+buildOp2 _ (Just rop) _  (I i) (R r') = Just $ rop (fromIntegral i) r'
+buildOp2 _ (Just rop) _  (R r) (I i') = Just $ rop r (fromIntegral i')
+buildOp2 _ (Just rop) _  (R r) (R r') = Just $ rop r r'
 -- floating
-buildOp2 _ _ (Just fop)  v     v'     = Just . F       $ (fop `on` asF) v v'
+buildOp2 _ _ (Just fop)  v     v'     = Just $ (fop `on` asF) v v'
 -- invalid types - fail
 buildOp2 _ _ _           _     _      = Nothing
 
 
 -- specialised op builders
 
-buildIOp1 iop = buildOp1 (Just iop) Nothing Nothing
-buildIOp2 iop = buildOp2 (Just iop) Nothing Nothing
+-- build an operator that remains in the type of it's operands
+buildOp1' iop rop fop = buildOp1 (fmap (I . ) iop) (fmap (reduceR . ) rop) (fmap (F . ) fop)
+buildOp2' iop rop fop = buildOp2 (fmap (I .:) iop) (fmap (reduceR .:) rop) (fmap (F .:) fop)
 
-buildFOp1 fop = buildOp1 Nothing Nothing (Just fop)
-buildFOp2 fop = buildOp2 Nothing Nothing (Just fop)
+-- build a total operator (handler for all types)
+buildTOp1  iop rop fop = buildOp1  (Just iop) (Just rop) (Just fop)
+buildTOp2  iop rop fop = buildOp2  (Just iop) (Just rop) (Just fop)
+buildTOp1' iop rop fop = buildOp1' (Just iop) (Just rop) (Just fop)
+buildTOp2' iop rop fop = buildOp2' (Just iop) (Just rop) (Just fop)
 
-buildOp1' iop rop fop = buildOp1 (Just iop) (Just rop) (Just fop)
-buildOp2' iop rop fop = buildOp2 (Just iop) (Just rop) (Just fop)
+buildIOp1' iop = buildOp1' (Just iop) Nothing Nothing
+buildIOp2' iop = buildOp2' (Just iop) Nothing Nothing
 
-buildRounder iop rop fop v
-  = Just . I $ case v of I i -> iop i
-                         R r -> rop r
-                         F f -> fop f
+buildFOp1' fop = buildOp1' Nothing Nothing (Just fop)
+buildFOp2' fop = buildOp2' Nothing Nothing (Just fop)
+
+buildRounder rop fop = buildTOp1 I (I . rop) (I . fop)
 
 
 -- operators
 
 -- abs
-opAbs = buildOp1' abs abs abs
+opAbs = buildTOp1' abs abs abs
 
 -- negation
 opNegate = Just . negateVal
 
 -- reciprocol
-opRecip = buildOp1 Nothing (Just recip) (Just recip)
+opRecip = buildOp1' Nothing (Just recip) (Just recip)
 
 -- square root
 opSqrt v | isNeg v = Nothing
-opSqrt v = buildFOp1 sqrt v
+opSqrt v = buildFOp1' sqrt v
 
 -- exp
-opExp = buildFOp1 exp
+opExp = buildFOp1' exp
 
 -- natural log
 opLn v | isNeg v || isZero v = Nothing
-opLn v = buildFOp1 log v
+opLn v = buildFOp1' log v
 
 -- log base 2
 opLog2 = opLog (I 2)
 
 -- factorial
 opFact v | isNeg v = Nothing
-opFact v = buildIOp1 fact v
+opFact v = buildIOp1' fact v
   where
     fact i = product [1..i]
 
 -- addition
-opAdd = buildOp2' (+) (+) (+)
+opAdd = buildTOp2' (+) (+) (+)
 
 -- subtraction
-opSubtract = buildOp2' (-) (-) (-)
+opSubtract = buildTOp2' (-) (-) (-)
 
 -- multiplication
-opMultiply = buildOp2' (*) (*) (*)
+opMultiply = buildTOp2' (*) (*) (*)
 
 -- real division
 opDivide _ v | isZero v = Nothing
-opDivide v v' = buildOp2 Nothing (Just (/)) (Just (/)) v v'
+opDivide v v' = buildOp2' Nothing (Just (/)) (Just (/)) v v'
 
 -- integer division
 opIDivide _ v | isZero v = Nothing
-opIDivide v v' = buildIOp2 div v v'
+opIDivide v v' = buildIOp2' div v v'
 
 -- modulo
 opMod _ v | isZero v = Nothing
-opMod v v' = buildOp2 (Just mod) Nothing (Just mod') v v'
+opMod v v' = buildOp2' (Just mod) Nothing (Just mod') v v'
 
 -- power
 opPower :: Operator2
@@ -172,40 +176,40 @@ opPower v (I i')  -- take advantage of an integer index
   | otherwise = Just $ case v of I i -> R $ fromIntegral i ^^ i'
                                  R r -> R $ r ^^ i'
                                  F f -> F $ f ^^ i'
-opPower v v' = buildFOp2 (**) v v'  -- use floating operation in other cases
+opPower v v' = buildFOp2' (**) v v'  -- use floating operation in other cases
 
 -- log
 opLog v _ | isNeg v || isZero v = Nothing
 opLog _ v | isNeg v || isZero v = Nothing
-opLog v v' = buildFOp2 (flip logBase) v v'  -- log_2(8) -> 8 2 log
+opLog v v' = buildFOp2' (flip logBase) v v'  -- log_2(8) -> 8 2 log
 
 -- trig
-opSin   = buildFOp1 sin
-opCos   = buildFOp1 cos
-opTan   = buildFOp1 tan
-opSinh  = buildFOp1 sinh
-opCosh  = buildFOp1 cosh
-opTanh  = buildFOp1 tanh
-opAsin  = buildFOp1 asin
-opAcos  = buildFOp1 acos
-opAtan  = buildFOp1 atan
-opAsinh = buildFOp1 asinh
-opAcosh = buildFOp1 acosh
-opAtanh = buildFOp1 atanh
+opSin   = buildFOp1' sin
+opCos   = buildFOp1' cos
+opTan   = buildFOp1' tan
+opSinh  = buildFOp1' sinh
+opCosh  = buildFOp1' cosh
+opTanh  = buildFOp1' tanh
+opAsin  = buildFOp1' asin
+opAcos  = buildFOp1' acos
+opAtan  = buildFOp1' atan
+opAsinh = buildFOp1' asinh
+opAcosh = buildFOp1' acosh
+opAtanh = buildFOp1' atanh
 
-opDeg = buildFOp1 ((`mod'` 360     ) . (* (180 / pi)))
-opRad = buildFOp1 ((`mod'` (2 * pi)) . (* (pi / 180)))
+opDeg = buildFOp1' ((`mod'` 360     ) . (* (180 / pi)))
+opRad = buildFOp1' ((`mod'` (2 * pi)) . (* (pi / 180)))
 
 -- bitwise
-opNot  = buildIOp1 complement
-opAnd  = buildIOp2 (.&.)
-opOr   = buildIOp2 (.|.)
-opNand = buildIOp2 (complement .: (.&.))
-opNor  = buildIOp2 (complement .: (.|.))
-opXor  = buildIOp2 xor
+opNot  = buildIOp1' complement
+opAnd  = buildIOp2' (.&.)
+opOr   = buildIOp2' (.|.)
+opNand = buildIOp2' (complement .: (.&.))
+opNor  = buildIOp2' (complement .: (.|.))
+opXor  = buildIOp2' xor
 
 -- misc
-opRnd   = buildRounder id round   round
-opFloor = buildRounder id floor   floor
-opCeil  = buildRounder id ceiling ceiling
+opRnd   = buildRounder round   round
+opFloor = buildRounder floor   floor
+opCeil  = buildRounder ceiling ceiling
 opFloat = Just . F . asF
